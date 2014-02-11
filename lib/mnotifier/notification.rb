a26b6b1986e21ff1e90793dfcb79459fd8a4cab3
Minwoo Lee <ermaker@gmail.com>
2014-02-11 14:52:10 +0900

    Make Sinatra related code be modular

--- lib/mnotifier/notification.rb
+++ lib/mnotifier/notification.rb
@@ -1,21 +1,23 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'digest/sha1'
 
-get '/notifications' do
-  <<-EOS
+class Notification < Sinatra::Base
+  get '/notifications' do
+    <<-EOS
 <form method=post enctype='multipart/form-data'>
   <input name=detail />
   <input type=submit />
 </form>
-  EOS
-end
-
-post '/notifications' do
-  contents = params[:detail]
-  filename = Digest::SHA1.hexdigest(contents)
-  FileUtils.mkdir_p 'notifications', :mode => 0700
-  File.open(File.join('notifications', filename), 'w', 0600) do |f|
-    f << contents
+    EOS
   end
-  "#{request.url}/#{filename}"
+
+  post '/notifications' do
+    contents = params[:detail]
+    filename = Digest::SHA1.hexdigest(contents)
+    FileUtils.mkdir_p 'notifications', :mode => 0700
+    File.open(File.join('notifications', filename), 'w', 0600) do |f|
+      f << contents
+    end
+    "#{request.url}/#{filename}"
+  end
 end
--- spec/lib/mnotifier/notification_spec.rb
+++ spec/lib/mnotifier/notification_spec.rb
@@ -10,7 +10,7 @@ describe 'MNotifier App' do
   include FakeFS::SpecHelpers
 
   def app
-    Sinatra::Application
+    Notification.new
   end
 
   it 'posts notifications' do
